32075fca02539104ac6170ee72037c65992fb7dd
Anderson <anderson21fonseca@gamil.com>
2022-10-25 17:42:54 -0300

    Primeiro commit

---
+++ javascript/anima.js
@@ -0,0 +1,25 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+ var a,b,c;
+ a=3;
+ b=5;
+ c=a+b;
+ console.log(c);
+ var letra= document.getElementById('caju').innerHTML="lua";
+ var cont =0;
+ function  java(){
+     cont++;
+    
+    if(cont===1){
+        alert("olá mundo");
+    }else if (cont===2){
+        alert("Segunda vez");
+        cont=0;
+   }
+   
+ }
